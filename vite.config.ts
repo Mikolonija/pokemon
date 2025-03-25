1708bd0a0986803111cc81432b8efe5585e6db98
Mikolonija <bieliauskasmodestas47@gmail.com>
2025-03-25 10:11:35 +0200

    install compression and implimented

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { compression } from "vite-plugin-compression2";
+
 import path from "path";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    compression({
+      algorithm: "gzip",
+      deleteOriginalAssets: false,
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
