fa4c33b38f12fdd1f3556c8d1879f76f3c7958be
Mikolonija <bieliauskasmodestas47@gmail.com>
2025-03-25 10:08:47 +0200

    made what all import files started @

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./styles/index.css";
-import App from "./pages/index.tsx";
+import "@/styles/index.css";
+import App from "@/pages/index.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
