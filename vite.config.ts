767518e7a8303e858a3bf3a3821283bc1d9328cc
Mikolonija <bieliauskasmodestas47@gmail.com>
2025-03-25 10:19:39 +0200

    delete eslint intall prettierc and formated files

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,22 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import { compression } from "vite-plugin-compression2";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import { compression } from 'vite-plugin-compression2';
 
-import path from "path";
+import path from 'path';
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
     compression({
-      algorithm: "gzip",
+      algorithm: 'gzip',
       deleteOriginalAssets: false,
     }),
   ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
