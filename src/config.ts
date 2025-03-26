c02f347b435e1bcc77eeb7fcaba64515be07b9ee
Mikolonija <bieliauskasmodestas47@gmail.com>
2025-03-26 12:30:17 +0200

    fix electric text color

--- src/config.ts
+++ src/config.ts
@@ -18,7 +18,7 @@ const POKEMON_TYPES: Record<string, PokemonTypeDetails> = {
   [PokemonType.FIRE]: { name: 'Fire', color: '#D32F2F', backgroundColor: '#F9A087' },
   [PokemonType.WATER]: { name: 'Water', color: '#1976D2', backgroundColor: '#A0C8F8' },
   [PokemonType.GRASS]: { name: 'Grass', color: '#388E3C', backgroundColor: '#B8E58C' },
-  [PokemonType.ELECTRIC]: { name: 'Electric', color: '#FBC02D', backgroundColor: '#F1E04E' },
+  [PokemonType.ELECTRIC]: { name: 'Electric', color: '#9E9E00', backgroundColor: '#F1E04E' },
   [PokemonType.PSYCHIC]: { name: 'Psychic', color: '#D81B60', backgroundColor: '#F8A3D2' },
   [PokemonType.ICE]: { name: 'Ice', color: '#0288D1', backgroundColor: '#D0F0F0' },
   [PokemonType.DRAGON]: { name: 'Dragon', color: '#512DA8', backgroundColor: '#D0A0F8' },
