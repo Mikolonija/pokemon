cff4ab3d65c1177bb3348b9056e2c26a50f37949
Mikolonija <bieliauskasmodestas47@gmail.com>
2025-03-27 09:59:34 +0200

    styles current pokemon then data not found also optmize picture

--- src/components/CurrentPokemon.tsx
+++ src/components/CurrentPokemon.tsx
@@ -6,12 +6,15 @@ import {
   PokemonSpecies,
   SelectedPokemon,
 } from '@/utils/interface/pokemon';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, startTransition } from 'react';
 import { useParams } from 'react-router';
+import notFoundPokemon from '@/assets/notFoundPokemon.png';
 
 const CurrentPokemon = () => {
   const { id } = useParams<{ id: string }>();
   const [selectedPokemon, setSelectedPokemon] = useState<SelectedPokemon | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     if (id) {
@@ -19,59 +22,93 @@ const CurrentPokemon = () => {
     }
   }, [id]);
 
-  const convertToPokemonID = (url: string) => {
-    const id = url.split('/')[6];
-    return Number(id);
-  };
+  const convertToPokemonID = (url: string) => Number(url.split('/')[6]);
 
   const getCurrentPokemon = async (pokemonID: number) => {
-    try {
-      const pokeDetails: PokemonDetails = await fetchPokemon(pokemonID);
-      const speciesResponse = await fetch(pokeDetails.species.url);
-      const speciesData: PokemonSpecies = await speciesResponse.json();
-      const evolutionResponse = await fetch(speciesData.evolution_chain.url);
-      const evolutionData: EvolutionChain = await evolutionResponse.json();
-      setSelectedPokemon({
-        details: pokeDetails,
-        text:
-          speciesData.flavor_text_entries.find((x) => x.language.name === 'en')?.flavor_text ||
-          'No description available.',
-        evolution: evolutionData,
-      });
-    } catch (err) {
-      console.error('Error fetching Pokémon details:', err);
-    }
+    setLoading(true);
+    setError(null);
+
+    startTransition(async () => {
+      try {
+        const pokeDetails: PokemonDetails = await fetchPokemon(pokemonID);
+        const speciesResponse = await fetch(pokeDetails.species.url);
+        const speciesData: PokemonSpecies = await speciesResponse.json();
+        const evolutionResponse = await fetch(speciesData.evolution_chain.url);
+        const evolutionData: EvolutionChain = await evolutionResponse.json();
+
+        setSelectedPokemon({
+          details: pokeDetails,
+          text:
+            speciesData.flavor_text_entries.find((x) => x.language.name === 'en')?.flavor_text ||
+            'No description available.',
+          evolution: evolutionData,
+        });
+      } catch (err) {
+        console.error('Error fetching Pokémon details:', err);
+        setError('Failed to load Pokémon details.');
+      } finally {
+        setLoading(false);
+      }
+    });
   };
+
   return (
-    <div className="fixed w-[300px] h-full  bg-(--card-color) shadow-lg rounded-3xl">
-      {selectedPokemon && (
-        <div className="p-6 text-center grid gap-4">
-          <div className="relative">
-            <img
-              className="absolute inset-x-0  mx-auto w-[130px]  bottom-0"
-              src={
-                selectedPokemon.details.sprites.versions['generation-v']['black-white'].animated
-                  .front_default
-              }
-              alt={selectedPokemon.details.name}
-            />
+    <div className="fixed w-[300px] h-full bg-(--card-color) shadow-lg rounded-3xl ">
+      {loading ? (
+        <div className="flex items-center justify-center  h-full">
+          <div className="loader"></div>
+        </div>
+      ) : error ? (
+        <div className="flex flex-col items-center justify-center h-full text-center">
+          <p className="text-(--card-description-color)">{error}</p>
+        </div>
+      ) : selectedPokemon ? (
+        <div className="p-6 text-center grid animate-slide-up">
+          {/* Pokemon Image */}
+          <div className="relative flex justify-center">
+            <div className="absolute bottom-0 h-[15vh] flex justify-center">
+              {selectedPokemon?.details?.sprites?.versions?.['generation-v']?.['black-white']
+                ?.animated?.front_default ? (
+                <img
+                  className="max-w-[200px] h-full object-contain"
+                  src={
+                    selectedPokemon.details.sprites.versions['generation-v']['black-white'].animated
+                      .front_default
+                  }
+                  alt={selectedPokemon.details.name}
+                />
+              ) : (
+                <img className="max-w-[200px] h-full object-contain" src={notFoundPokemon} alt="" />
+              )}
+            </div>
           </div>
+
+          {/* Pokemon Info */}
           <p>N0{selectedPokemon.details.id}</p>
-          <p>{selectedPokemon.details.name}</p>
-          <p>{selectedPokemon.details.types.map((x) => x.type.name + ' ')}</p>
-          <p>Pokedex Entry</p>
-          <p>{selectedPokemon.text}</p>
-          <h1>Height</h1>
+          <p className="capitalize">{selectedPokemon.details.name}</p>
+          <p>{selectedPokemon.details.types.map((x) => x.type.name).join(', ')}</p>
+
+          {/* Pokedex Entry */}
+          <div>
+            <p className="font-semibold">Pokedex Entry</p>
+            <p>{selectedPokemon.text}</p>
+          </div>
+
+          {/* Stats */}
+          <h1 className="font-semibold">Height</h1>
           <p>{selectedPokemon.details.height}</p>
-          <h1>Weight</h1>
+          <h1 className="font-semibold">Weight</h1>
           <p>{selectedPokemon.details.weight}</p>
-          <h1>Abilities</h1>
-          <p>{selectedPokemon.details.abilities.map((x) => x.ability.name + ' ')}</p>
-          <h1>Stats</h1>
-          <p>{selectedPokemon.details.stats.map((x) => x.base_stat + ' ')}</p>
-          <h1>Evolution</h1>
+          <h1 className="font-semibold">Abilities</h1>
+          <p>{selectedPokemon.details.abilities.map((x) => x.ability.name).join(', ')}</p>
+          <h1 className="font-semibold">Stats</h1>
+          <p>{selectedPokemon.details.stats.map((x) => x.base_stat).join(', ')}</p>
+
+          {/* Evolution */}
+          <h1 className="font-semibold">Evolution</h1>
           <p>
             <img
+              className="cursor-pointer"
               onClick={() =>
                 getCurrentPokemon(convertToPokemonID(selectedPokemon.evolution.chain.species.url))
               }
@@ -82,30 +119,25 @@ const CurrentPokemon = () => {
               selectedPokemon.evolution.chain.evolves_to.map(
                 (evolution: EvolutionChain, index: number) => (
                   <span key={index}>
-                    {
-                      <>
-                        <span>{'Lv.' + (evolution.evolution_details[0]?.min_level ?? '?')}</span>
-                      </>
-                    }
+                    <span>{'Lv.' + (evolution.evolution_details[0]?.min_level ?? '?')}</span>
                     <img
+                      className="cursor-pointer"
                       onClick={() => getCurrentPokemon(convertToPokemonID(evolution.species.url))}
                       src={`${POKEMON_IMG_PATH}${evolution.species.url.split('/')[6]}.png`}
                       alt=""
                     />
-                    {evolution.evolves_to && evolution.evolves_to.length > 0 && (
-                      <>
-                        {evolution.evolves_to.map((evo: EvolutionChain, i: number) => (
-                          <span key={i}>
-                            <span> {'Lv.' + (evo.evolution_details[0].min_level ?? '?')}</span>
-                            <img
-                              onClick={() => getCurrentPokemon(convertToPokemonID(evo.species.url))}
-                              src={`${POKEMON_IMG_PATH}${evo.species.url.split('/')[6]}.png`}
-                              alt=""
-                            />
-                          </span>
-                        ))}
-                      </>
-                    )}
+                    {evolution.evolves_to.length > 0 &&
+                      evolution.evolves_to.map((evo: EvolutionChain, i: number) => (
+                        <span key={i}>
+                          <span>{'Lv.' + (evo.evolution_details[0]?.min_level ?? '?')}</span>
+                          <img
+                            className="cursor-pointer"
+                            onClick={() => getCurrentPokemon(convertToPokemonID(evo.species.url))}
+                            src={`${POKEMON_IMG_PATH}${evo.species.url.split('/')[6]}.png`}
+                            alt=""
+                          />
+                        </span>
+                      ))}
                   </span>
                 ),
               )
@@ -113,6 +145,19 @@ const CurrentPokemon = () => {
               <span>No evolutions available.</span>
             )}
           </p>
+        </div>
+      ) : (
+        <div className="p-6 text-center grid gap-4">
+          <div className="relative flex justify-center">
+            <div className="absolute bottom-0 h-[15vh] flex justify-center">
+              <img className="max-w-[200px] h-full object-contain" src={notFoundPokemon} alt="" />
+            </div>
+          </div>
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            <h1 className="text-md font-bold text-(--card-description-color)">
+              Select a Pokémon to display here.
+            </h1>
+          </div>
         </div>
       )}
     </div>
